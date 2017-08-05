4d1088eda3c1d0a6c3b60e6f957cfda61d250be1
Joachim Lusiardi <joachim@lusiardi.de>
2017-08-05 11:03:36 +0200

    adds first HAP function
    Adds funtions for perform_pair_setup and get_session_keys
    Adds SRP implementation

---
+++ homekit/protocol.py
@@ -0,0 +1,291 @@
+import sys
+import hkdf
+import hashlib
+import ed25519
+import py25519
+from binascii import hexlify
+from homekit.tlv import TLV
+from homekit.srp import SrpClient
+from homekit.chacha20poly1305 import chacha20_aead_decrypt, chacha20_aead_encrypt
+
+
+def perform_pair_setup(connection, pin, ios_pairing_id):
+    """
+    Performs a pair setup operation as described in chapter 4.7 page 39 ff.
+
+    :param connection: the http connection to the target accessory
+    :param pin: the setup code from the accessory
+    :param ios_pairing_id: the id of the simulated ios device
+    :return: a dict with the ios device's part of the pairing information
+    """
+    headers = {
+        'Content-Type': 'application/pairing+tlv8'
+    }
+
+    #
+    # Step #1 ios --> accessory (send SRP start Request) (see page 39)
+    #
+    request_tlv = TLV.encode_dict({
+        TLV.kTLVType_State: TLV.M1,
+        TLV.kTLVType_Method: TLV.PairSetup
+    })
+
+    connection.request('POST', '/pair-setup', request_tlv, headers)
+    resp = connection.getresponse()
+    response_tlv = TLV.decode_bytes(resp.read())
+
+    #
+    # Step #3 ios --> accessory (send SRP verify request) (see page 41)
+    #
+    assert TLV.kTLVType_State in response_tlv, response_tlv
+    assert response_tlv[TLV.kTLVType_State] == TLV.M2
+    if TLV.kTLVType_Error in response_tlv:
+        if response_tlv[TLV.kTLVType_Error] == TLV.kTLVError_Unavailable:
+            print('Step #3: kTLVError_Unavailable!')
+        elif response_tlv[TLV.kTLVType_Error] == TLV.kTLVError_MaxTries:
+            print('Step #3: kTLVError_MaxTries!')
+        else:
+            print('Step #3: unkown error!')
+        sys.exit(-1)
+
+    srp_client = SrpClient('Pair-Setup', pin)
+    srp_client.set_salt(response_tlv[TLV.kTLVType_Salt])
+    srp_client.set_server_public_key(response_tlv[TLV.kTLVType_PublicKey])
+    client_pub_key = srp_client.get_public_key()
+    client_proof = srp_client.get_proof()
+
+    response_tlv = TLV.encode_dict({
+        TLV.kTLVType_State: TLV.M3,
+        TLV.kTLVType_PublicKey: SrpClient.to_byte_array(client_pub_key),
+        TLV.kTLVType_Proof: SrpClient.to_byte_array(client_proof),
+    })
+
+    connection.request('POST', '/pair-setup', response_tlv, headers)
+    resp = connection.getresponse()
+    response_tlv = TLV.decode_bytes(resp.read())
+
+    #
+    # Step #5 ios --> accessory (Exchange Request) (see page 43)
+    #
+
+    # M4 Verification (page 43)
+    assert TLV.kTLVType_State in response_tlv, response_tlv
+    assert response_tlv[TLV.kTLVType_State] == TLV.M4
+    if TLV.kTLVType_Error in response_tlv:
+        if response_tlv[TLV.kTLVType_Error] == TLV.kTLVError_Authentication:
+            print('Step #5: kTLVError_Authentication!')
+        else:
+            print('Step #5: unkown error!')
+        sys.exit(-1)
+
+    assert TLV.kTLVType_Proof in response_tlv
+    if not srp_client.verify_servers_proof(response_tlv[TLV.kTLVType_Proof]):
+        print('Step #5: wrong proof!')
+
+    # M5 Request generation (page 44)
+    session_key = srp_client.get_session_key()
+
+    ios_device_ltsk, ios_device_ltpk = ed25519.create_keypair()
+
+    # reversed:
+    #   Pair-Setup-Encrypt-Salt instead of Pair-Setup-Controller-Sign-Salt
+    #   Pair-Setup-Encrypt-Info instead of Pair-Setup-Controller-Sign-Info
+    hkdf_inst = hkdf.Hkdf('Pair-Setup-Controller-Sign-Salt'.encode(), SrpClient.to_byte_array(session_key),
+                          hash=hashlib.sha512)
+    ios_device_x = hkdf_inst.expand('Pair-Setup-Controller-Sign-Info'.encode(), 32)
+
+    hkdf_inst = hkdf.Hkdf('Pair-Setup-Encrypt-Salt'.encode(), SrpClient.to_byte_array(session_key),
+                          hash=hashlib.sha512)
+    session_key = hkdf_inst.expand('Pair-Setup-Encrypt-Info'.encode(), 32)
+
+    ios_device_pairing_id = ios_pairing_id.encode()
+    ios_device_info = ios_device_x + ios_device_pairing_id + ios_device_ltpk.to_bytes()
+
+    ios_device_signature = ios_device_ltsk.sign(ios_device_info)
+
+    sub_tlv = {
+        TLV.kTLVType_Identifier: ios_device_pairing_id,
+        TLV.kTLVType_PublicKey: ios_device_ltpk.to_bytes(),
+        TLV.kTLVType_Signature: ios_device_signature
+    }
+    sub_tlv_b = TLV.encode_dict(sub_tlv)
+
+    # taking tge iOSDeviceX as key was reversed from
+    # https://github.com/KhaosT/HAP-NodeJS/blob/2ea9d761d9bd7593dd1949fec621ab085af5e567/lib/HAPServer.js
+    # function handlePairStepFive calling encryption.encryptAndSeal
+    encrypted_data_with_auth_tag = chacha20_aead_encrypt(bytes(), session_key, 'PS-Msg05'.encode(), bytes([0, 0, 0, 0]),
+                                                         sub_tlv_b)
+    tmp = bytearray(encrypted_data_with_auth_tag[0])
+    tmp += encrypted_data_with_auth_tag[1]
+
+    response_tlv = {
+        TLV.kTLVType_State: TLV.M5,
+        TLV.kTLVType_EncryptedData: tmp
+    }
+    body = TLV.encode_dict(response_tlv)
+
+    connection.request('POST', '/pair-setup', body, headers)
+    resp = connection.getresponse()
+    response_tlv = TLV.decode_bytes(resp.read())
+
+    #
+    # Step #7 ios (Verification) (page 47)
+    #
+    assert response_tlv[TLV.kTLVType_State] == TLV.M6
+    if TLV.kTLVType_Error in response_tlv:
+        if response_tlv[TLV.kTLVType_Error] == TLV.kTLVError_Authentication:
+            print('Step #7: kTLVError_Authentication!')
+        elif response_tlv[TLV.kTLVType_Error] == TLV.kTLVError_MaxPeers:
+            print('Step #7: kTLVError_MaxPeers!')
+        else:
+            print('Step #7: unkown error!')
+        sys.exit(-1)
+
+    assert TLV.kTLVType_EncryptedData in response_tlv
+    decrypted_data = chacha20_aead_decrypt(bytes(), session_key, 'PS-Msg06'.encode(), bytes([0, 0, 0, 0]),
+                                           response_tlv[TLV.kTLVType_EncryptedData])
+    if decrypted_data == False:
+        print('Step #7: Abort because of illegal data')
+        sys.exit(-1)
+
+    response_tlv = TLV.decode_bytearray(decrypted_data)
+    assert TLV.kTLVType_Signature in response_tlv
+    accessory_sig = response_tlv[TLV.kTLVType_Signature]
+
+    assert TLV.kTLVType_Identifier in response_tlv
+    accessory_pairing_id = response_tlv[TLV.kTLVType_Identifier]
+
+    assert TLV.kTLVType_PublicKey in response_tlv
+    accessory_ltpk = response_tlv[TLV.kTLVType_PublicKey]
+    hkdf_inst = hkdf.Hkdf('Pair-Setup-Accessory-Sign-Salt'.encode(),
+                          SrpClient.to_byte_array(srp_client.get_session_key()),
+                          hash=hashlib.sha512)
+    accessory_x = hkdf_inst.expand('Pair-Setup-Accessory-Sign-Info'.encode(), 32)
+
+    accessory_info = accessory_x + accessory_pairing_id + accessory_ltpk
+
+    e25519s = ed25519.VerifyingKey(bytes(response_tlv[TLV.kTLVType_PublicKey]))
+    e25519s.verify(bytes(accessory_sig), bytes(accessory_info))
+
+    return {
+        'AccessoryPairingID': response_tlv[TLV.kTLVType_Identifier].decode(),
+        'AccessoryLTPK': hexlify(response_tlv[TLV.kTLVType_PublicKey]).decode(),
+        'iOSPairingId': ios_pairing_id,
+        'iOSDeviceLTSK': ios_device_ltsk.to_ascii(encoding='hex').decode(),
+        'iOSDeviceLTPK': hexlify(ios_device_ltpk.to_bytes()).decode()
+    }
+
+
+def get_session_keys(conn, pairing_data):
+    """
+    Performs a pair verify operation as described in chapter 4.8 page 47 ff.
+
+    :param conn: the http connection to the target accessory
+    :param pairing_data: the paring data as returned by perform_pair_setup
+    :return: tuple of the session keys (controller_to_accessory_key and  accessory_to_controller_key)
+    """
+    #
+    # Step #1 ios --> accessory (send verify start Request) (page 47)
+    #
+    ios_key = py25519.Key25519()
+
+    request_tlv = TLV.encode_dict({
+        TLV.kTLVType_State: TLV.M1,
+        TLV.kTLVType_PublicKey: ios_key.pubkey
+    })
+
+    conn.request('POST', '/pair-verify', request_tlv)
+    resp = conn.getresponse()
+    response_tlv = TLV.decode_bytes(resp.read())
+
+    #
+    # Step #3 ios --> accessory (send SRP verify request)  (page 49)
+    #
+    assert TLV.kTLVType_State in response_tlv, response_tlv
+    assert response_tlv[TLV.kTLVType_State] == TLV.M2
+    assert TLV.kTLVType_PublicKey in response_tlv, response_tlv
+    assert TLV.kTLVType_EncryptedData in response_tlv, response_tlv
+
+    # 1) generate shared secret
+    accessorys_session_pub_key_bytes = response_tlv[TLV.kTLVType_PublicKey]
+    shared_secret = ios_key.get_ecdh_key(
+        py25519.Key25519(pubkey=bytes(accessorys_session_pub_key_bytes), verifyingkey=bytes()))
+
+    # 2) derive session key
+    hkdf_inst = hkdf.Hkdf('Pair-Verify-Encrypt-Salt'.encode(), shared_secret, hash=hashlib.sha512)
+    session_key = hkdf_inst.expand('Pair-Verify-Encrypt-Info'.encode(), 32)
+
+    # 3) verify authtag on encrypted data and 4) decrypt
+    encrypted = response_tlv[TLV.kTLVType_EncryptedData]
+    decrypted = chacha20_aead_decrypt(bytes(), session_key, 'PV-Msg02'.encode(), bytes([0, 0, 0, 0]),
+                                      encrypted)
+    if decrypted == False:
+        print('Step #3: authtag was wrong')
+        sys.exit(-1)
+    d1 = TLV.decode_bytes(decrypted)
+    assert TLV.kTLVType_Identifier in d1
+    assert TLV.kTLVType_Signature in d1
+
+    # 5) look up pairing by accessory name
+    accessory_name = d1[TLV.kTLVType_Identifier].decode()
+
+    if pairing_data['AccessoryPairingID'] != accessory_name:
+        print('Step #3: No pair_setup was performed (or wrong pairing file)')
+        sys.exit(-1)
+
+    accessory_ltpk = py25519.Key25519(pubkey=bytes(), verifyingkey=bytes.fromhex(pairing_data['AccessoryLTPK']))
+
+    # 6) verify accessory's signature
+    accessory_sig = d1[TLV.kTLVType_Signature]
+    accessory_session_pub_key_bytes = response_tlv[TLV.kTLVType_PublicKey]
+    accessory_info = accessory_session_pub_key_bytes + accessory_name.encode() + ios_key.pubkey
+    if not accessory_ltpk.verify(bytes(accessory_sig), bytes(accessory_info)):
+        print('Step #3: Signature was invalid')
+        sys.exit(-1)
+
+    # 7) create iOSDeviceInfo
+    ios_device_info = ios_key.pubkey + pairing_data['iOSPairingId'].encode() + accessorys_session_pub_key_bytes
+
+    # 8) sign iOSDeviceInfo with long term secret key
+    ios_device_ltsk_h = pairing_data['iOSDeviceLTSK']
+    ios_device_ltsk = py25519.Key25519(secretkey=bytes.fromhex(ios_device_ltsk_h))
+    ios_device_signature = ios_device_ltsk.sign(ios_device_info)
+
+    # 9) construct sub tlv
+    sub_tlv = TLV.encode_dict({
+        TLV.kTLVType_Identifier: pairing_data['iOSPairingId'].encode(),
+        TLV.kTLVType_Signature: ios_device_signature
+    })
+
+    # 10) encrypt and sign
+    encrypted_data_with_auth_tag = chacha20_aead_encrypt(bytes(), session_key, 'PV-Msg03'.encode(), bytes([0, 0, 0, 0]),
+                                                         sub_tlv)
+    tmp = bytearray(encrypted_data_with_auth_tag[0])
+    tmp += encrypted_data_with_auth_tag[1]
+
+    # 11) create tlv
+    request_tlv = TLV.encode_dict({
+        TLV.kTLVType_State: TLV.M3,
+        TLV.kTLVType_EncryptedData: tmp
+    })
+
+    # 12) send to accessory
+    conn.request('POST', '/pair-verify', request_tlv)
+    resp = conn.getresponse()
+    response_tlv = TLV.decode_bytes(resp.read())
+
+    #
+    #   Post Step #4 verification (page 51)
+    #
+    assert TLV.kTLVType_State in response_tlv
+    assert response_tlv[TLV.kTLVType_State] == TLV.M4
+    assert TLV.kTLVType_Error not in response_tlv, response_tlv[TLV.kTLVType_Error]
+
+    # calculate session keys
+    hkdf_inst = hkdf.Hkdf('Control-Salt'.encode(), shared_secret, hash=hashlib.sha512)
+    controller_to_accessory_key = hkdf_inst.expand('Control-Write-Encryption-Key'.encode(), 32)
+
+    hkdf_inst = hkdf.Hkdf('Control-Salt'.encode(), shared_secret, hash=hashlib.sha512)
+    accessory_to_controller_key = hkdf_inst.expand('Control-Read-Encryption-Key'.encode(), 32)
+
+    return controller_to_accessory_key, accessory_to_controller_key
---
+++ homekit/srp.py
@@ -0,0 +1,282 @@
+# -*- coding: UTF-8 -*-
+"""
+Implements the Secure Remote Password (SRP) algorithm. More information can be found on
+https://tools.ietf.org/html/rfc5054. See HomeKit spec page 36 for adjustments imposed by Apple.
+"""
+
+import crypt
+import gmpy2
+import hashlib
+
+import six
+import binascii
+
+
+class Srp:
+    def __init__(self):
+        # generator as defined by 3072bit group of RFC 5054
+        self.g = gmpy2.mpz(int(six.b('5'), 16))
+        # modulus as defined by 3072bit group of RFC 5054
+        self.n = gmpy2.mpz(int(six.b('''\
+FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E08\
+8A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B\
+302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9\
+A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE6\
+49286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8\
+FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D\
+670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C\
+180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF695581718\
+3995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D\
+04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7D\
+B3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D226\
+1AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200C\
+BBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFC\
+E0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF'''), 16))
+        # HomeKit requires SHA-512 (See page 36)
+        self.h = hashlib.sha512
+
+    def _calculate_k(self) -> gmpy2.mpz:
+        # calculate k (see https://tools.ietf.org/html/rfc5054#section-2.5.3)
+        hash_instance = self.h()
+        n = Srp.to_byte_array(self.n)
+        g = bytearray.fromhex((383 * '00' + '05'))  # 383 * b'0' + '5'.encode()
+        hash_instance.update(n)
+        hash_instance.update(g)
+        k = gmpy2.mpz(int(binascii.hexlify(hash_instance.digest()), 16))
+        return k
+
+    def _calculate_u(self) -> gmpy2.mpz:
+        if self.A is None:
+            raise RuntimeError('Client\'s public key is missing')
+        if self.B is None:
+            raise RuntimeError('Server\'s public key is missing')
+        hash_instance = self.h()
+        A_b = Srp.to_byte_array(self.A)
+        B_b = Srp.to_byte_array(self.B)
+        hash_instance.update(A_b)
+        hash_instance.update(B_b)
+        u = gmpy2.mpz(int(binascii.hexlify(hash_instance.digest()), 16))
+        return u
+
+    def get_session_key(self) -> int:
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.get_shared_secret()))
+        hash_value = int(binascii.hexlify(hash_instance.digest()), 16)
+        return hash_value
+
+    @staticmethod
+    def to_byte_array(num: int) -> bytearray:
+        h = gmpy2.digits(num, 16)
+        if len(h) % 2 == 1:
+            h = '0' + h
+        return bytearray.fromhex(h)
+
+    def _calculate_x(self) -> int:
+        i = (self.username + ':' + self.password).encode()
+        hash_instance = self.h()
+        hash_instance.update(i)
+        hash_value = hash_instance.digest()
+
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.salt))
+        hash_instance.update(hash_value)
+
+        return int(binascii.hexlify(hash_instance.digest()), 16)
+
+
+class SrpClient(Srp):
+    """
+    Implements all functions that are required to simulate an iOS HomeKit controller
+    """
+    def __init__(self, username: str, password: str):
+        Srp.__init__(self)
+        self.username = username
+        self.password = password
+        self.salt = None
+        self.a = gmpy2.mpz(int(binascii.hexlify(crypt.mksalt(crypt.METHOD_SHA512)[3:].encode()), 16))
+        self.A = pow(self.g, self.a, self.n)
+        self.B = None
+
+    def set_salt(self, salt):
+        if isinstance(salt, bytearray):
+            self.salt = gmpy2.mpz(salt.hex(), 16)
+        else:
+            self.salt = salt
+
+    def get_public_key(self) -> gmpy2.mpz:
+        return pow(self.g, self.a, self.n)
+
+    def set_server_public_key(self, B):
+        if isinstance(B, bytearray):
+            self.B = gmpy2.mpz(B.hex(), 16)
+        else:
+            self.B = B
+
+    def get_shared_secret(self):
+        if self.B is None:
+            raise RuntimeError('Server\'s public key is missing')
+        u = self._calculate_u()
+        x = self._calculate_x()
+        k = self._calculate_k()
+        tmp1 = (self.B - (k * pow(self.g, x, self.n)))
+        tmp2 = (self.a + (u * x))  # % self.n
+        S = pow(tmp1, tmp2, self.n)
+        return S
+
+    def get_proof(self):
+        if self.B is None:
+            raise RuntimeError('Server\'s public key is missing')
+
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.n))
+        hN = bytearray(hash_instance.digest())
+
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.g))
+        hg = bytearray(hash_instance.digest())
+
+        for index in range(0, len(hN)):
+            hN[index] ^= hg[index]
+
+        u = self.username.encode()
+        hash_instance = self.h()
+        hash_instance.update(u)
+        hu = hash_instance.digest()
+        K = Srp.to_byte_array(self.get_session_key())
+
+        hash_instance = self.h()
+        hash_instance.update(hN)
+        hash_instance.update(hu)
+        hash_instance.update(Srp.to_byte_array(self.salt))
+        hash_instance.update(Srp.to_byte_array(self.A))
+        hash_instance.update(Srp.to_byte_array(self.B))
+        hash_instance.update(K)
+        r = binascii.hexlify(hash_instance.digest())
+        return int(r, 16)
+
+    def verify_servers_proof(self, M):
+        if isinstance(M, bytearray):
+            tmp = gmpy2.mpz(M.hex(), 16)
+        else:
+            tmp = M
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.A))
+        hash_instance.update(Srp.to_byte_array(self.get_proof()))
+        hash_instance.update(Srp.to_byte_array(self.get_session_key()))
+        return tmp == gmpy2.mpz(binascii.hexlify(hash_instance.digest()), 16)
+
+
+class SrpServer(Srp):
+    """
+    Implements all functions that are required to simulate an iOS HomeKit accessory
+    """
+    def __init__(self, username, password):
+        Srp.__init__(self)
+        self.username = username
+        self.salt = SrpServer._create_salt()
+        self.password = password
+        self.verifier = self._get_verifier()
+        salt = crypt.mksalt(crypt.METHOD_SHA256)[3:].encode()
+        salt_b = binascii.hexlify(salt)
+        self.b = gmpy2.mpz(int(salt_b, 16))
+        k = self._calculate_k()
+        g_b = pow(self.g, self.b, self.n)
+        self.B = (k * self.verifier + g_b) % self.n
+
+        self.A = None
+
+    @staticmethod
+    def _create_salt() -> gmpy2.mpz:
+        salt = crypt.mksalt(crypt.METHOD_SHA512)[3:]
+        salt_b = salt.encode()
+        salt_hex = binascii.hexlify(salt_b)
+        salt_int = int(salt_hex, 16)
+        assert len(salt) == 16
+        return gmpy2.mpz(salt_int)
+
+    def _get_verifier(self) -> int:
+        hash_value = self._calculate_x()
+        v = pow(self.g, hash_value, self.n)
+        return v
+
+    def set_client_public_key(self, A):
+        self.A = A
+
+    def get_salt(self):
+        return self.salt
+
+    def get_public_key(self):
+        k = self._calculate_k()
+        return (k * self.verifier + pow(self.g, self.b, self.n)) % self.n
+
+    def get_shared_secret(self):
+        if self.A is None:
+            raise RuntimeError('Client\'s public key is missing')
+
+        tmp1 = self.A * pow(self.verifier, self._calculate_u(), self.n)
+        return pow(tmp1, self.b, self.n)
+
+    def verify_clients_proof(self, m) -> bool:
+        if self.B is None:
+            raise RuntimeError('Server\'s public key is missing')
+
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.n))
+        hN = bytearray(hash_instance.digest())
+
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.g))
+        hg = bytearray(hash_instance.digest())
+
+        for index in range(0, len(hN)):
+            hN[index] ^= hg[index]
+
+        u = self.username.encode()
+        hash_instance = self.h()
+        hash_instance.update(u)
+        hu = hash_instance.digest()
+        K = Srp.to_byte_array(self.get_session_key())
+
+        hash_instance = self.h()
+        hash_instance.update(hN)
+        hash_instance.update(hu)
+        hash_instance.update(Srp.to_byte_array(self.salt))
+        hash_instance.update(Srp.to_byte_array(self.A))
+        hash_instance.update(Srp.to_byte_array(self.B))
+        hash_instance.update(K)
+        r = binascii.hexlify(hash_instance.digest())
+        return m == gmpy2.mpz(r, 16)
+
+    def get_proof(self, m) -> gmpy2.mpz:
+        hash_instance = self.h()
+        hash_instance.update(Srp.to_byte_array(self.A))
+        hash_instance.update(Srp.to_byte_array(m))
+        hash_instance.update(Srp.to_byte_array(self.get_session_key()))
+        return gmpy2.mpz(binascii.hexlify(hash_instance.digest()), 16)
+
+
+if __name__ == '__main__':
+    # step M1
+
+    # step M2
+    setup_code = '123-45-678'  # transmitted on second channel
+    server = SrpServer('Pair-Setup', setup_code)
+    server_pub_key = server.get_public_key()
+    server_salt = server.get_salt()
+
+    # step M3
+    client = SrpClient('Pair-Setup', setup_code)
+    client.set_salt(server_salt)
+    client.set_server_public_key(server_pub_key)
+
+    client_pub_key = client.get_public_key()
+    clients_proof = client.get_proof()
+
+    # step M4
+    server.set_client_public_key(client_pub_key)
+    server_shared_secret = server.get_shared_secret()
+    assert server.verify_clients_proof(clients_proof)
+    servers_proof = server.get_proof(clients_proof)
+
+    # step M5
+    assert client.verify_servers_proof(servers_proof)

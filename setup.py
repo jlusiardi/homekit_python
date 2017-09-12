a452b2238fd213b34fffdc25019f9e8e9152b0c8
Joachim Lusiardi <joachim@lusiardi.de>
2017-09-12 10:14:03 +0200

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='homekit',
     packages=['homekit', 'homekit.model'],  # this must be the same as the name above
-    version='0.1',
+    version='0.2',
     description='Python code to interface HomeKit Accessories and Controllers',
     author='Joachim Lusiardi',
     author_email='pypi@lusiardi.de',
     url='https://github.com/jlusiardi/homekit_python',  # use the URL to the github repo
-    download_url='https://github.com/jlusiardi/homekit_python/archive/0.1.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/jlusiardi/homekit_python/archive/0.2.tar.gz',  # I'll explain this in a second
     keywords=['HomeKit'],  # arbitrary keywords
     classifiers=[],
     install_requires=[

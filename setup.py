9e666e836f59b8dd6bd8f4ea366767f640cc793a
Joachim Lusiardi <joachim@lusiardi.de>
2017-10-29 08:18:34 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='homekit',
     packages=['homekit', 'homekit.model'],  # this must be the same as the name above
-    version='0.3',
+    version='0.4',
     description='Python code to interface HomeKit Accessories and Controllers',
     author='Joachim Lusiardi',
     author_email='pypi@lusiardi.de',
     url='https://github.com/jlusiardi/homekit_python',  # use the URL to the github repo
-    download_url='https://github.com/jlusiardi/homekit_python/archive/0.3.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/jlusiardi/homekit_python/archive/0.4.tar.gz',  # I'll explain this in a second
     keywords=['HomeKit'],  # arbitrary keywords
     classifiers=[],
     install_requires=[

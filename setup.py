21032e73a67e8aa68c969a57352673b567740750
Joachim Lusiardi <joachim@lusiardi.de>
2018-03-19 06:36:47 +0100

    adjust setup file

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ from distutils.core import setup
 setup(
     name='homekit',
     packages=['homekit', 'homekit.model'],  # this must be the same as the name above
-    version='0.4',
+    version='0.5',
     description='Python code to interface HomeKit Accessories and Controllers',
     author='Joachim Lusiardi',
     author_email='pypi@lusiardi.de',
     url='https://github.com/jlusiardi/homekit_python',  # use the URL to the github repo
-    download_url='https://github.com/jlusiardi/homekit_python/archive/0.4.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/jlusiardi/homekit_python/archive/0.5.tar.gz',  # I'll explain this in a second
     keywords=['HomeKit'],  # arbitrary keywords
     classifiers=[],
     install_requires=[

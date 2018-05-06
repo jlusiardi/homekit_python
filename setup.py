d01d11d3931efc682d0c3a0a77a9c32f6a2418b1
Joachim Lusiardi <jlusiardi@users.noreply.github.com>
2018-05-06 17:26:16 +0200

    adjust setup file for 0.7

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ from distutils.core import setup
 setup(
     name='homekit',
     packages=['homekit', 'homekit.model'],  # this must be the same as the name above
-    version='0.6',
+    version='0.7',
     description='Python code to interface HomeKit Accessories and Controllers',
     author='Joachim Lusiardi',
     author_email='pypi@lusiardi.de',
-    url='https://github.com/jlusiardi/homekit_python',  # use the URL to the github repo
-    download_url='https://github.com/jlusiardi/homekit_python/archive/0.6.tar.gz',  # I'll explain this in a second
-    keywords=['HomeKit'],  # arbitrary keywords
+    url='https://github.com/jlusiardi/homekit_python',  
+    download_url='https://github.com/jlusiardi/homekit_python/archive/0.7.tar.gz',  
+    keywords=['HomeKit'],  
     classifiers=[],
     install_requires=[
         'zeroconf',

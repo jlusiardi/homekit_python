ea471eb2f1910c604564fb63fae11287f6bac02d
Joachim Lusiardi <joachim@lusiardi.de>
2018-05-19 08:42:13 +0200

    improve handling of multiple characteristics and the documentation

--- homekit/put_characteristic.py
+++ homekit/put_characteristic.py
@@ -25,10 +25,11 @@ from homekit import SecureHttp, load_pairing, HapStatusCodes, save_pairing, crea
 
 
 def setup_args_parser():
-    parser = argparse.ArgumentParser(description='HomeKit perform app - performs operations on paired devices')
+    parser = argparse.ArgumentParser(description='HomeKit put_characteristic app - change values of characteristics ' +
+                                                 'on paired HomeKit accessories.')
     parser.add_argument('-f', action='store', required=True, dest='file', help='File with the pairing data')
-    parser.add_argument('-c', action='store', required=False, dest='characteristics')
-    parser.add_argument('-v', action='store', required=False, dest='value')
+    parser.add_argument('-c', action='append', required=False, dest='characteristics', nargs=2,
+                        help='Use aid.iid value to change the value. Repeat to change multiple characteristics.')
 
     return parser
 
@@ -48,49 +49,52 @@ if __name__ == '__main__':
     parser = setup_args_parser()
     args = parser.parse_args()
 
-    if not args.characteristics:
+    if 'characteristics' not in args or not args.characteristics:
         parser.print_help()
         sys.exit(-1)
-    if not args.value:
-        parser.print_help()
-        sys.exit(-1)
-
-    tmp = args.characteristics.split('.')
-    aid = int(tmp[0])
-    iid = int(tmp[1])
-    value = args.value
 
     conn, controllerToAccessoryKey, accessoryToControllerKey = create_session(args.file)
     sec_http = SecureHttp(conn.sock, accessoryToControllerKey, controllerToAccessoryKey)
 
     pairing_data = load_pairing(args.file)
-    # first check if the accessories data is in the paring data
-    characteristic_type = None
-    if 'accessories' not in pairing_data or not get_format(pairing_data, aid, iid):
-        # nope, so get it via /accessories and save it
-        response = sec_http.get('/accessories')
-        data = json.loads(response.read().decode())
-        pairing_data['accessories'] = data['accessories']
-        save_pairing(args.file, pairing_data)
-    # after loading the accessories data the aid.iid should be there...
-    characteristic_type = get_format(pairing_data, aid, iid)
-    if not characteristic_type:
-        print('Characteristic {aid}.{iid} not found'.format(aid=aid, iid=iid))
-        sys.exit(-1)
 
-    # reformat the value to fit the required format
-    if characteristic_type == 'bool':
-        try:
-            value = strtobool(value)
-        except ValueError:
-            print('{v} is no valid boolean!'.format(v=value))
+    # args.characteristics contains a list of lists like [['1.10', 'on'], ['1.11', '50']]
+    characteristics = []
+    for characteristic in args.characteristics:
+        # extract aid, iid and value from cli params
+        tmp = characteristic[0].split('.')
+        aid = int(tmp[0])
+        iid = int(tmp[1])
+        value = characteristic[1]
+
+        # first check if the accessories data is in the paring data
+        characteristic_type = None
+        if 'accessories' not in pairing_data or not get_format(pairing_data, aid, iid):
+            # nope, so get it via /accessories and save it
+            response = sec_http.get('/accessories')
+            data = json.loads(response.read().decode())
+            pairing_data['accessories'] = data['accessories']
+            save_pairing(args.file, pairing_data)
+        # after loading the accessories data the aid.iid should be there...
+        characteristic_type = get_format(pairing_data, aid, iid)
+        if not characteristic_type:
+            print('Characteristic {aid}.{iid} not found'.format(aid=aid, iid=iid))
             sys.exit(-1)
 
-    # TODO more conversion according to Table 5-5 page 67 required
-    else:
-        pass
+        # reformat the value to fit the required format
+        if characteristic_type == 'bool':
+            try:
+                value = strtobool(value)
+            except ValueError:
+                print('{v} is no valid boolean!'.format(v=value))
+                sys.exit(-1)
+        # TODO more conversion according to Table 5-5 page 67 required
+        else:
+            pass
 
-    body = json.dumps({'characteristics': [{'aid': aid, 'iid': iid, 'value': value}]})
+        characteristics.append({'aid': aid, 'iid': iid, 'value': value})
+
+    body = json.dumps({'characteristics': characteristics})
     response = sec_http.put('/characteristics', body)
     data = response.read().decode()
     if response.code != 204:

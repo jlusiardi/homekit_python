4832ec51d8bc75f9662c4868e0f015db672ad098
Joachim Lusiardi <joachim@lusiardi.de>
2018-05-23 21:50:08 +0200

    adjust setup file for 0.8

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ from distutils.core import setup
 setup(
     name='homekit',
     packages=['homekit', 'homekit.model'],  # this must be the same as the name above
-    version='0.7',
+    version='0.8',
     description='Python code to interface HomeKit Accessories and Controllers',
     author='Joachim Lusiardi',
     author_email='pypi@lusiardi.de',
     url='https://github.com/jlusiardi/homekit_python',  
-    download_url='https://github.com/jlusiardi/homekit_python/archive/0.7.tar.gz',  
+    download_url='https://github.com/jlusiardi/homekit_python/archive/0.8.tar.gz',
     keywords=['HomeKit'],  
     classifiers=[],
     install_requires=[

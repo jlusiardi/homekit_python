44a771e99e6743e7bcedff28e9b440b5281869d4
Joachim Lusiardi <joachim@lusiardi.de>
2018-07-14 12:58:42 +0200

    add license to tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,21 @@
+#
+# Copyright 2018 Joachim Lusiardi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from tests.tlv_test import TestTLV
 from tests.srp_test import TestSrp
 from tests.chacha20poly1305_test import TestChacha20poly1305
 from tests.serverdata_test import TestServerData
+from tests.http_response_test import TestHttpResponse

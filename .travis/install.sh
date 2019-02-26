d4be33fff29f299aadee892c2442f8cadc8c47ad
Joachim Lusiardi <joachim@lusiardi.de>
2019-02-26 07:12:44 +0100

    add installer script for python on OS X

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 echo "OS: $TRAVIS_OS_NAME"
+
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     sudo apt-get update;
     sudo apt-get install -y build-essential python3-dev libdbus-1-dev libdbus-glib-1-dev libgirepository1.0-dev;
     pip install coveralls;
     pip install flake8;
+fi
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update
+    # see https://github.com/pyenv/pyenv/wiki#suggested-build-environment for Mac OS X
+    brew install openssl readline sqlite3 xz zlib
+    brew install pyenv
+    pyenv install $PYTHON
 fi

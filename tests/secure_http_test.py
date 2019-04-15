033338275cad5756b929159e647440841ae1c570
Joachim Lusiardi <joachim@lusiardi.de>
2019-04-15 18:49:12 +0200

    add some tests for long requests

--- tests/secure_http_test.py
+++ tests/secure_http_test.py
@@ -60,7 +60,7 @@ class ResponseProvider(threading.Thread):
             cnt_bytes = self.a2c_counter.to_bytes(8, byteorder='little')
             self.a2c_counter += 1
             ciper_and_mac = chacha20_aead_encrypt(len_bytes, self.a2c_key, cnt_bytes, bytes([0, 0, 0, 0]),
-                                                data.encode())
+                                                  data.encode())
 
             if self.encryption_fail:
                 ciper_and_mac[0][0] = 0
@@ -179,3 +179,25 @@ class TestSecureHttp(unittest.TestCase):
         accessory_socket.close()
         self.assertEqual(200, result.code)
         self.assertEqual(bytearray(b' ' * 1025), result.body)
+
+    def test_get_large_request(self):
+        controller_socket, accessory_socket = socket.socketpair()
+
+        key_c2a = b'S2}\xb1}-l\n\x83\xe5}\'U\xc0\x1b\x0f\x08%X\xfdu\x1f\x9el/\x9bZ"\xec5\xa5P'
+        key_a2c = b'\x16\xab\xd3\xfe\x95{\xe56\x1fH\x81\xfd\x914\xa0@\xaa\x0e\xa6\xebw\xf2\xe3w:\x11/\x01\xbb;,\x1d'
+
+        tthread = ResponseProvider(accessory_socket, key_c2a, key_a2c)
+        tthread.start()
+
+        with mock.patch('homekit.controller.ip_implementation.IpSession') as session:
+            session.sock = controller_socket
+            session.a2c_key = key_a2c
+            session.c2a_key = key_c2a
+
+            sh = SecureHttp(session, timeout=10)
+            result = sh.get('/' + 1024 * 'a')
+
+        controller_socket.close()
+        accessory_socket.close()
+        self.assertEqual(200, result.code)
+        self.assertEqual(b'', result.body)

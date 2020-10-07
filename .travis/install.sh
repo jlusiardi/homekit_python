f8b0cef4a2bc488adb0c36559d007a57bebc149b
Joachim Lusiardi <joachim@lusiardi.de>
2020-10-07 21:56:34 +0200

    no special openssl anymore

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,10 +4,10 @@ echo "OS: $TRAVIS_OS_NAME"
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     # update openssl to a version that is sufficient for cryptography 2.6 (openssl 1.1 is required since)
-    wget http://launchpadlibrarian.net/400343104/libssl1.1_1.1.0g-2ubuntu4.3_amd64.deb
-    sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4.3_amd64.deb
-    wget http://launchpadlibrarian.net/367327834/openssl_1.1.0g-2ubuntu4_amd64.deb
-    sudo dpkg -i openssl_1.1.0g-2ubuntu4_amd64.deb
+    #wget http://launchpadlibrarian.net/400343104/libssl1.1_1.1.0g-2ubuntu4.3_amd64.deb
+    #sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4.3_amd64.deb
+    #wget http://launchpadlibrarian.net/367327834/openssl_1.1.0g-2ubuntu4_amd64.deb
+    #sudo dpkg -i openssl_1.1.0g-2ubuntu4_amd64.deb
     openssl version
     sudo apt-get update;
     sudo apt-get install -y build-essential python3-dev libdbus-1-dev libdbus-glib-1-dev libgirepository1.0-dev;

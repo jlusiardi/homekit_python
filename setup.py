7e3fbb0b11f38fbe2358ececf98048325c125165
Joachim Lusiardi <joachim@lusiardi.de>
2020-12-14 07:42:57 +0100

    Fix OS X builds and Pin version of Zeroconf

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         'tlv8>=0.9.0',
     ],
     extras_require={
-        'IP': ['zeroconf'],
+        'IP': ['zeroconf<=0.28.6'],
         'BLE': ['dbus-python', 'gatt', 'pygobject']
     },
     license='Apache License 2.0',

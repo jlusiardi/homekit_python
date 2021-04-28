f692778b5ce677f7225d1d2b883c57230b7d039a
Omer Nevo <omernevo@gmail.com>
2021-04-28 11:11:36 +0300

    add support for a general device identified as tv

---
+++ homekit/model/characteristics/active.py
@@ -0,0 +1,32 @@
+#
+# Copyright 2021 Omer Nevo
+# Added to homekit Copyright 2018 Joachim Lusiardi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from homekit.model.characteristics import CharacteristicsTypes, CharacteristicFormats, AbstractCharacteristic
+
+
+class ActiveCharacteristic(AbstractCharacteristic):
+    def __init__(self, iid):
+        AbstractCharacteristic.__init__(self, iid, CharacteristicsTypes.ACTIVE, CharacteristicFormats.uint8)
+        self.valid_values = {
+          0: "Inactive",
+          1: "Active"
+        }
+        self.name = "Active"
+        self.description = "Active"
+        self.format = "uint8"
+        self.perms = ['pr', 'pw', 'ev']
+        self.type = "000000B0-0000-1000-8000-0026BB765291"

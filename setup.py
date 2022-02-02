53bc0a236ecab3c57ed9f16dfcc4a5c8a41ff676
Joachim Lusiardi <joachim@lusiardi.de>
2022-02-02 09:01:33 +0100

    restrict zeroconf to version 0.32.0
    newer versions drop 'Zeroconf.check_service'. See
    https://github.com/jlusiardi/homekit_python/issues/225

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         'tlv8>=0.9.0',
     ],
     extras_require={
-        'IP': ['zeroconf'],
+        'IP': ['zeroconf==0.32.0'],
         'BLE': ['dbus-python', 'gatt', 'pygobject']
     },
     license='Apache License 2.0',
